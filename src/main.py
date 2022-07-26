0a46f5f065317d6a89f6f36d433097af4c345030
Brenda Lopez <brenda.lopez.2@aptiv.com>
2022-07-26 12:02:54 -0500

    add setter and getter in motor class

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from carro import Carro
 
 if __name__ == '__main__':
     carro = Carro('susuki swift', 'gray', 'susuki', 4)
-    
-    print(vars(carro))
+    carro._motor.temperatura = -10
+    print(carro._motor.temperatura)
